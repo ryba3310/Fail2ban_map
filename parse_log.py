d5f3a8e4fe2310ff344c83cac75e5aec3c0fd3bc
Michal Ryba <michal8876@gmail.com>
2025-03-24 18:39:55 +0000

    Added main ORM functionality and refactored the code

--- parse_log.py
+++ parse_log.py
@@ -1,4 +1,6 @@
 from signal import signal, SIGINT
+from json.decoder import JSONDecodeError
+from collections import namedtuple
 import requests
 import time
 import psycopg2
@@ -10,6 +12,7 @@ LOG_PATH = '/app/fail2ban.log'
 IP_API = 'http://ip-api.com/json/'
 POSTGRES_USER = POSTGRES_DB = os.environ['POSTGRES_USER']    # Postgres Docker image sets db name as users name by default
 POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
+
 
 try:
     db = PostgresqlDatabase(
@@ -59,8 +62,12 @@ def create_tables():
 
 
 def get_ip_data(ip):
-    host_metadata = requests.get(f'{IP_API}{ip}')
-    return host_metadata.json()
+    try:
+        host_metadata = requests.get(f'{IP_API}{ip}')
+        return host_metadata.json()
+    except JSONDecodeError as e:
+        print('\t[get_ip_data] Exeeded requests per minute limit')
+        return None
 
 
 def insert_host(host_metadata, attempt_data):
@@ -69,39 +76,18 @@ def insert_host(host_metadata, attempt_data):
     print(" Date: " + attempt_data[1])
     ################# ORM
     try:
-        client = (Client.insert(ip=attempt_data[0], country=host_metadata['country'], region_name=host_metadata['region'],
+        client = (Client.insert(ip=attempt_data.ip, country=host_metadata['country'], region_name=host_metadata['region'],
                                city=host_metadata['city'], lat=host_metadata['lat'], lon=host_metadata['lon'],
-                               isp=host_metadata['isp'], org=host_metadata['org'], asn=host_metadata['as'], date=attempt_data[1])
-                  .on_conflict(conflict_target=[Client.ip], update={Client.lat: host_metadata['lat']})
+                               isp=host_metadata['isp'], org=host_metadata['org'], asn=host_metadata['as'], date=attempt_data.timestamp)
+                    .on_conflict(conflict_target=[Client.ip], update={Client.lat: host_metadata['lat']})
                       .execute())
-        attempt = Attempt.create(client=client, date=attempt_data[1])
+        attempt = Attempt.insert(client=client, attempted_at=attempt_data.timestamp).execute()
+        if attempt_data.banned:
+            ban = Ban.insert(client=client, jail=attempt_data.jail, banned_at=attempt_data.timestamp).execute()
+            print(ban)
     except IntegrityError as err:
-        print('Client already inserted, updateing attempt...\n')
-        #attempt = Attempt.create(client=client, date=attempt_data[1])
-        #attempt = Attempt.update(client=client, date=date).where(Client.ip == attempt_data[0])
-        #if attempt_data[3]:
-        #    ban = Ban.create(client=client, jail=attempt_data[2], banned_at=attempt_data[1])
-#    try:
-#        cur.execute('INSERT INTO f2b (ip, banned, country, region_name, city, lat, lon, isp, org, asn, date, jail, attempts_num) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)\
-#            ON CONFLICT (id) DO UPDATE SET banned = excluded.banned, date = excluded.date',\
-#            (attempt_data[0], attempt_data[3], host_metadata['country'], host_metadata['regionName'], host_metadata['city'], host_metadata['lat'], host_metadata['lon'], host_metadata['isp'], \
-#            host_metadata['org'], host_metadata['as'], attempt_data[1], attempt_data[2], '1'))
-#        conn.commit()
-#    except Exception as err:
-#        print('Cannot insert')
-#        print(err)
+        print('\t[insert_host]Couldn\'t insert data...\n')
 
-############### Fix function name
-def check_if_inserted(attempt_data):
-    ###############ORM
-    cur.execute('SELECT ip FROM f2b WHERE date >= %s', (attempt_data[1],))
-    record = cur.fetchone()
-    if record == None:
-        return False
-    #################FIX and ORM
-    # Update attempt count if host read from log is already inserted
-    cur.execute('UPDATE f2b SET attempts_num = attempts_num + 1 WHERE date < %s AND ip = %s', (attempt_data[1], attempt_data[0]))
-    return True
 
 def follow_generator(log_file):
     """Generator which first reads all the lines then simulates tail -f behaviour"""
@@ -111,34 +97,30 @@ def follow_generator(log_file):
             time.sleep(0.1)
         yield line
 
-def get_ip_timestamp_jail(line):
-    line = ' '.join(line.split()).split(' ')
-    ip = line[7]
-    timestamp = line[0] + ' ' + line[1][0:8]
-    jail = line[5]
-    ############# Return tuple
-    return [ip, timestamp, jail]
 
 def parse_line(line):
     """Only look for Bans and Attemps(Notice) in log and save host and timestamp"""
-    ip_timestamp_jail = []
-    if 'Found' in line:
-        ip_timestamp_jail = get_ip_timestamp_jail(line)
-        ip_timestamp_jail.append('false')
-    elif 'Ban' in line:
-        ip_timestamp_jail = get_ip_timestamp_jail(line)
-        ip_timestamp_jail.append('true')
-    else:
-        return None
-    # tuple = (ip_addr, timestamp, jail_name, banned(boolean))
-    ################# the var will be tuple from function call
-    return tuple(ip_timestamp_jail)
+
+    banned = False
+    Attempt_data = namedtuple('Attempt_data', ['ip', 'timestamp', 'jail', 'banned'])
+    if 'Ban' in line:
+        banned = True
+    if 'Found' in line or banned:
+        line = ' '.join(line.split()).split(' ')
+        ip = line[7]
+        timestamp = line[0] + ' ' + line[1][0:8]
+        jail = line[5]
+        attempt_data = Attempt_data(ip, timestamp, jail, banned)
+        return attempt_data
+
+    return None
+
 
 def handle_SIGINT(sig, frame):
-    print('Gracefully closing...')
-    cur.close()
-    conn.close()
+    print('\nClosing db connection...')
+    db.close()
     exit(0)
+
 
 if __name__ == '__main__':
     with open(LOG_PATH) as log_file:
@@ -150,13 +132,13 @@ if __name__ == '__main__':
             attempt_data = parse_line(line)
             if not attempt_data:
                 continue
-        #    if check_if_inserted(attempt_data):
-        #        continue
             host_metadata = get_ip_data(attempt_data[0])
-            insert_host(host_metadata, attempt_data)
             rate_limit += 1
-            if rate_limit > 40:
+            if rate_limit > 40 or host_metadata == None:
                 time.sleep(60)
                 rate_limit = 0
+                host_metadata = get_ip_data(attempt_data[0])
+            insert_host(host_metadata, attempt_data)
+
     cur.close()
     conn.close()
